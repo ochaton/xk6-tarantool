f17ad0fabb43b22c9764f79f395f587f783c1007
Vladislav Grubov <vladgrubov@gmail.com>
2022-04-27 20:53:37 +0400

    support ConnectionPool

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/hackfeed/xk6-tarantool
 go 1.16
 
 require (
-	github.com/tarantool/go-tarantool v0.0.0-20210330210617-56fe55c5fa5c
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/tarantool/go-tarantool v0.0.0-20220425140629-e9b9ba1ed26a
 	go.k6.io/k6 v0.32.0
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
--- tarantool.go
+++ tarantool.go
@@ -2,6 +2,7 @@ package tarantool
 
 import (
 	"github.com/tarantool/go-tarantool"
+	ttcp "github.com/tarantool/go-tarantool/connection_pool"
 	"go.k6.io/k6/js/modules"
 )
 
@@ -13,19 +14,27 @@ func init() {
 type Tarantool struct{}
 
 // Connect creates a new Tarantool connection
-func (Tarantool) Connect(addr string, opts tarantool.Opts) (*tarantool.Connection, error) {
+func (t Tarantool) Connect(addr string, opts tarantool.Opts) (*ttcp.ConnectionPool, error) {
 	if addr == "" {
 		addr = "localhost:3301"
 	}
-	conn, err := tarantool.Connect(addr, opts)
+	return t.ConnectPool([]string{addr}, opts)
+}
+
+// ConnectPool connects to shard of Tarantools
+func (Tarantool) ConnectPool(addrs []string, opts tarantool.Opts) (*ttcp.ConnectionPool, error) {
+	if len(addrs) == 0 {
+		addrs = []string{"localhost:3301"}
+	}
+	pool, err := ttcp.Connect(addrs, opts)
 	if err != nil {
 		return nil, err
 	}
-	return conn, nil
+	return pool, nil
 }
 
-// Select performs select to box.space
-func (Tarantool) Select(conn *tarantool.Connection, space, index interface{}, offset, limit, iterator uint32, key interface{}) (*tarantool.Response, error) {
+// Select performs select to box.space (any node)
+func (Tarantool) Select(conn *ttcp.ConnectionPool, space, index interface{}, offset, limit, iterator uint32, key interface{}) (*tarantool.Response, error) {
 	resp, err := conn.Select(space, index, offset, limit, iterator, key)
 	if err != nil {
 		return nil, err
@@ -33,8 +42,26 @@ func (Tarantool) Select(conn *tarantool.Connection, space, index interface{}, of
 	return resp, err
 }
 
+// Select performs select to box.space (any node but prefer RW)
+func (Tarantool) SelectARW(conn *ttcp.ConnectionPool, space, index interface{}, offset, limit, iterator uint32, key interface{}) (*tarantool.Response, error) {
+	resp, err := conn.Select(space, index, offset, limit, iterator, key, ttcp.PreferRW)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
+// Select performs select to box.space (any node but prefer RO)
+func (Tarantool) SelectARO(conn *ttcp.ConnectionPool, space, index interface{}, offset, limit, iterator uint32, key interface{}) (*tarantool.Response, error) {
+	resp, err := conn.Select(space, index, offset, limit, iterator, key, ttcp.PreferRO)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
 // Insert performs insertion to box.space
-func (Tarantool) Insert(conn *tarantool.Connection, space, data interface{}) (*tarantool.Response, error) {
+func (Tarantool) Insert(conn *ttcp.ConnectionPool, space, data interface{}) (*tarantool.Response, error) {
 	resp, err := conn.Insert(space, data)
 	if err != nil {
 		return nil, err
@@ -43,7 +70,7 @@ func (Tarantool) Insert(conn *tarantool.Connection, space, data interface{}) (*t
 }
 
 // Replace performs "insert or replace" action to box.space
-func (Tarantool) Replace(conn *tarantool.Connection, space, data interface{}) (*tarantool.Response, error) {
+func (Tarantool) Replace(conn *ttcp.ConnectionPool, space, data interface{}) (*tarantool.Response, error) {
 	resp, err := conn.Replace(space, data)
 	if err != nil {
 		return nil, err
@@ -52,7 +79,7 @@ func (Tarantool) Replace(conn *tarantool.Connection, space, data interface{}) (*
 }
 
 // Delete performs deletion of a tuple by key
-func (Tarantool) Delete(conn *tarantool.Connection, space, index, key interface{}) (*tarantool.Response, error) {
+func (Tarantool) Delete(conn *ttcp.ConnectionPool, space, index, key interface{}) (*tarantool.Response, error) {
 	resp, err := conn.Delete(space, index, key)
 	if err != nil {
 		return nil, err
@@ -61,7 +88,7 @@ func (Tarantool) Delete(conn *tarantool.Connection, space, index, key interface{
 }
 
 // Update performs update of a tuple by key
-func (Tarantool) Update(conn *tarantool.Connection, space, index, key, ops interface{}) (*tarantool.Response, error) {
+func (Tarantool) Update(conn *ttcp.ConnectionPool, space, index, key, ops interface{}) (*tarantool.Response, error) {
 	resp, err := conn.Update(space, index, key, ops)
 	if err != nil {
 		return nil, err
@@ -70,7 +97,7 @@ func (Tarantool) Update(conn *tarantool.Connection, space, index, key, ops inter
 }
 
 // Upsert performs "update or insert" action of a tuple by key
-func (Tarantool) Upsert(conn *tarantool.Connection, space, tuple, ops interface{}) (*tarantool.Response, error) {
+func (Tarantool) Upsert(conn *ttcp.ConnectionPool, space, tuple, ops interface{}) (*tarantool.Response, error) {
 	resp, err := conn.Upsert(space, tuple, ops)
 	if err != nil {
 		return nil, err
@@ -78,18 +105,63 @@ func (Tarantool) Upsert(conn *tarantool.Connection, space, tuple, ops interface{
 	return resp, err
 }
 
-// Call calls registered tarantool function
-func (Tarantool) Call(conn *tarantool.Connection, fnName string, args interface{}) (*tarantool.Response, error) {
-	resp, err := conn.Call(fnName, args)
+// Call calls registered tarantool function (calls on any but prefer RW)
+func (Tarantool) Call(conn *ttcp.ConnectionPool, fnName string, args interface{}) (*tarantool.Response, error) {
+	resp, err := conn.Call(fnName, args, ttcp.PreferRW)
 	if err != nil {
 		return nil, err
 	}
 	return resp, err
 }
 
-// Call17 calls registered tarantool function
-func (Tarantool) Call17(conn *tarantool.Connection, fnName string, args interface{}) (*tarantool.Response, error) {
-	resp, err := conn.Call17(fnName, args)
+// Call calls registered tarantool function (calls on RW)
+func (Tarantool) CallARW(conn *ttcp.ConnectionPool, fnName string, args interface{}) (*tarantool.Response, error) {
+	resp, err := conn.Call(fnName, args, ttcp.PreferRW)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
+// Call calls registered tarantool function (calls on any but prefer RO)
+func (Tarantool) CallARO(conn *ttcp.ConnectionPool, fnName string, args interface{}) (*tarantool.Response, error) {
+	resp, err := conn.Call(fnName, args, ttcp.PreferRO)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
+// Call17 calls registered tarantool function (Call17 on any but prefer RW)
+func (Tarantool) Call17(conn *ttcp.ConnectionPool, fnName string, args interface{}) (*tarantool.Response, error) {
+	resp, err := conn.Call17(fnName, args, ttcp.PreferRW)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
+// Call17 calls registered tarantool function (Call17 on any but prefer RW)
+func (Tarantool) Call17ARO(conn *ttcp.ConnectionPool, fnName string, args interface{}) (*tarantool.Response, error) {
+	resp, err := conn.Call17(fnName, args, ttcp.PreferRO)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
+// Call17 calls registered tarantool function (Call17 on any but prefer RW)
+func (Tarantool) Call17RW(conn *ttcp.ConnectionPool, fnName string, args interface{}) (*tarantool.Response, error) {
+	resp, err := conn.Call17(fnName, args, ttcp.RW)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
+// Eval passes lua expression for evaluation (calls on any but prefers RW)
+func (Tarantool) Eval(conn *ttcp.ConnectionPool, expr string, args interface{}) (*tarantool.Response, error) {
+	resp, err := conn.Eval(expr, args, ttcp.PreferRW)
 	if err != nil {
 		return nil, err
 	}
@@ -97,8 +169,26 @@ func (Tarantool) Call17(conn *tarantool.Connection, fnName string, args interfac
 }
 
 // Eval passes lua expression for evaluation
-func (Tarantool) Eval(conn *tarantool.Connection, expr string, args interface{}) (*tarantool.Response, error) {
-	resp, err := conn.Eval(expr, args)
+func (Tarantool) EvalARW(conn *ttcp.ConnectionPool, expr string, args interface{}) (*tarantool.Response, error) {
+	resp, err := conn.Eval(expr, args, ttcp.PreferRW)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
+// Eval passes lua expression for evaluation (calls on any but prefers RO)
+func (Tarantool) EvalARO(conn *ttcp.ConnectionPool, expr string, args interface{}) (*tarantool.Response, error) {
+	resp, err := conn.Eval(expr, args, ttcp.PreferRO)
+	if err != nil {
+		return nil, err
+	}
+	return resp, err
+}
+
+// Eval passes lua expression for evaluation (calls on RW)
+func (Tarantool) EvalRW(conn *ttcp.ConnectionPool, expr string, args interface{}) (*tarantool.Response, error) {
+	resp, err := conn.Eval(expr, args, ttcp.RW)
 	if err != nil {
 		return nil, err
 	}
